a5a777ca6ab5760f804d6c90a1513ee6312f3609
Martin Vilar <38569826+mvilar2018@users.noreply.github.com>
2021-01-11 14:25:00 +0100

    pre_processing merge 2 files into 1

--- pre_processing1.py
+++ pre_processing1.py
@@ -20,3 +20,18 @@ def pre_process1(csv_filepath):
 
     return good_read
 
+def pre_pro2():
+    da = df["avg_rating"]
+    normalized_da =  (1 + (da - da.mean())/ (da.max() - da.min()) * 9
+    normalized_df_max_min = 1 + (da - da.min())/ (da.max() - da.min()) * 9
+    da["norm_mean"] = normalized_da
+    da["norm_max_min"] = normalized_df_max_min
+    return normalized_df
+    return normalized_df_max_min
+
+
+def count_awards(s):
+    #takes string s and counts the "," returns the count plus 1
+    return s.count(",") + 1
+
+print(count_awards("ocsar, bafta, somthing, good book"))
