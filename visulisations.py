24b587a0950218aa15c21dccbce0cd0ed04c402a
Joby Ingram-Dodd <joby@protonmail.com>
2021-01-12 11:44:31 +0000

    plots

--- visulisations.py
+++ visulisations.py
@@ -9,10 +9,11 @@ import scipy.stats as st
 # 9.
 def plot_ratings_year():
     df = pd.read_csv('data/analyse_this.csv')
-    pdf = df[['norm_mean',"original_publish_year"]]
+    pdf = df[['avg_rating',"original_publish_year"]]
     pdf = pdf[pdf['original_publish_year']<2020]
-    pdf = pdf.sort_values(by='original_publish_year')
-    pdf.plot(x='norm_mean',y="original_publish_year", kind = 'scatter')
+    pdf = pdf[pdf['original_publish_year']>200]
+    pdfg = pdf.groupby(['original_publish_year']).agg(ratings =('avg_rating','mean'))
+    pdfg.plot( kind = 'line', title="Ratings by year")
     plt.show()
 
 # 10.
@@ -20,10 +21,17 @@ def awards_ratings():
     df = pd.read_csv('data/analyse_this.csv')
     pdf = df[['norm_max_min',"awards"]]
     pdf["awards"]=pdf["awards"].fillna(0)
-    pdf.plot(x="awards",y="norm_max_min",kind="scatter")
+    pdfg = pdf.groupby(["awards"]).agg(ratings =('norm_max_min','mean'))
+    print(pdfg.head())
+    alt_plot_for_Awards_ratings(pdfg)
+    #fig, ax = plt.subplots()
+
+    #pdfg.plot.scatter(x='awards',y='ratings')
+    #plt.show()
+
+def alt_plot_for_Awards_ratings(pdfg):
+    pdfg.plot(kind='bar', title="Awards Vs Ratings")
     plt.show()
-
-
-plot_ratings_year()
+#plot_ratings_year()
 awards_ratings()
 
