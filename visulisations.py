f1a51c6c111963d187f7a7d2558972638d754c88
Ibrahim Animashaun <iaanimashaun@yahoo.com>
2021-01-12 15:40:35 +0000

    updated solution to question 1,2 and 3

--- visulisations.py
+++ visulisations.py
@@ -63,7 +63,8 @@ plt.show()
 
 pages_ratings_corr_coef = calc_corr_coef(df, 'num_pages', 'num_ratings')
 
-print(f'The correlation coeficient between number of pages and number of ratings is {pages_ratings_corr_coef}. This shows a very weak correlation between number of pages and number of ratings')
+print(f'The correlation coefficient between number of pages and number of ratings is {pages_ratings_corr_coef}.')
+print(f'This shows a very weak correlation between number of pages and number of ratings. This is also clearly illustrated in the scatterplot') 
 
 
 
