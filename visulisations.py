79b814891aec5e7e2836e11ed2c77e417c0e9b52
Joby Ingram-Dodd <joby@protonmail.com>
2021-01-12 09:14:48 +0000

    Update visulisations.py

--- visulisations.py
+++ visulisations.py
@@ -1,3 +1,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import scipy.stats as st
+
+# 1. Create a 2D scatterplot with `pages` on the x-axis and `num_ratings` on the y-axis.
+# 2. Can you compute numerically the correlation coefficient of these two columns?
+
