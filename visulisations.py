0df1fe3b1f159ac1d7a2e28338508c7d4a6ad8fc
Joby <56961121+jobyid@users.noreply.github.com>
2021-01-12 15:21:14 +0000

    Update visulisations.py

--- visulisations.py
+++ visulisations.py
@@ -15,6 +15,7 @@ def plot_ratings_year():
     pdf = pdf[pdf['original_publish_year']>200]
     pdfg = pdf.groupby(['original_publish_year']).agg(ratings =('avg_rating','mean'))
     pdfg.plot( kind = 'line', title="Ratings by year")
+    plt.savefig("ratings_vs_year.png")
     plt.show()
 
 # 10.
@@ -27,9 +28,11 @@ def awards_ratings():
     pdfg.reset_index(inplace=True)
     pdfg.plot(kind="scatter", x="awards", y="ratings", title="Awards vs Ratings")
     print(pdfg.head())
+    plt.savefig("ratings_vs_awards.png")
     plt.show()
 def alt_plot_for_Awards_ratings(pdfg):
     pdfg.plot(kind='bar', title="Awards Vs Ratings")
+    plt.savefig("ratings_vs_awards_alt.png")
     plt.show()
 
 #plot_ratings_year()
