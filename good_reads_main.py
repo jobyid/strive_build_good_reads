f7d28484ae0ab4a3322b916a4fb5876cac0f1b87
Ibrahim Animashaun <iaanimashaun@yahoo.com>
2021-01-13 15:43:54 +0000

    updated main

--- good_reads_main.py
+++ good_reads_main.py
@@ -58,7 +58,3 @@ if __name__ == '__main__':
 
 
 
-#Question 2
-pages_ratings_corr_coef = grv.calc_corr_coef(df, 'num_pages', 'num_ratings')
-print(f'The correlation coefficient between number of pages and number of ratings is {pages_ratings_corr_coef}.')
-print(f'This shows a very weak correlation between number of pages and number of ratings. This is also clearly illustrated in the scatterplot') 
