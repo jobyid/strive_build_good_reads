8c0df7c7827fe40a748b26d9488a5c1c984540a6
Joby Ingram-Dodd <joby@protonmail.com>
2021-01-13 16:02:23 +0000

    Update good_reads_visulisation.py

--- good_reads_visulisation.py
+++ good_reads_visulisation.py
@@ -77,36 +77,6 @@ def vis_all_norm(da):
 
 
 
-# 9.
-def plot_ratings_year():
-    df = pd.read_csv('data/analyse_this.csv')
-    pdf = df[['avg_rating',"original_publish_year"]]
-    pdf = pdf[pdf['original_publish_year']<2020]
-    pdf = pdf[pdf['original_publish_year']>200]
-    pdfg = pdf.groupby(['original_publish_year']).agg(ratings =('avg_rating','mean'))
-    pdfg.plot( kind = 'line', title="Ratings by year")
-    plt.savefig("ratings_vs_year.png")
-    plt.show()
-
-# 10.
-def awards_ratings():
-    df = pd.read_csv('data/analyse_this.csv')
-    pdf = df[['norm_max_min',"awards"]]
-    pdf["awards"]=pdf["awards"].fillna(0)
-    pdfg = pdf.groupby(["awards"]).agg(ratings =('norm_max_min','mean'))
-    alt_plot_for_Awards_ratings(pdfg)
-    pdfg.reset_index(inplace=True)
-    pdfg.plot(kind="scatter", x="awards", y="ratings", title="Awards vs Ratings")
-    print(pdfg.head())
-    plt.savefig("ratings_vs_awards.png")
-    plt.show()
-    
-    
-def alt_plot_for_Awards_ratings(pdfg):
-    pdfg.plot(kind='bar', title="Awards Vs Ratings")
-    plt.savefig("ratings_vs_awards_alt.png")
-    plt.show()
-
 
     
 def scatterplot_2d(df):
