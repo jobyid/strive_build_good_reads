665159b69599532cf1f89f7d2237f4c11e8cb02b
Joby Ingram-Dodd <joby@protonmail.com>
2021-01-13 10:52:28 +0000

    Visulisations

---
+++ good_reads_main.py
@@ -0,0 +1,10 @@
+import good_reads_visulisation as grv
+import sys
+
+
+def visulise():
+    grv.ratings_per_year_joint_plot()
+
+
+if __name__ == '__main__':
+    print("This is main what would you like to run?")
