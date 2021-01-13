ee36886d43f83953ad852102959b85b2508abda9
Oluseyi Oyedemi <oluseyi.oyedemi@gmail.com>
2021-01-13 22:49:56 +0200

    Update good_reads_analyse.py

--- good_reads_analyse.py
+++ good_reads_analyse.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 df = pd.read_csv('data/analyse_this.csv')
-
+# question number 7
 def my_best_book(author):
     autors_books = df[df['author']== author]
     rating = autors_books.loc[autors_books['norm_max_min'].idxmax()]
