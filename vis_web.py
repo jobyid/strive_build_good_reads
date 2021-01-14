9231c0b89382026c00c7f2c48273cc7336f8bd13
Joby Ingram-Dodd <joby@protonmail.com>
2021-01-14 14:22:37 +0000

    vis

---
+++ vis_web.py
@@ -0,0 +1,10 @@
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+import good_reads_analyse as gra
+import time
+
+st.subheader("Visualise the Data")
+
