5e12f6e0f71f9412fa03818cbb1fef666d88d63d
Joby Ingram-Dodd <joby@protonmail.com>
2021-01-14 14:56:00 +0000

    visulisation

--- analyse_website.py
+++ analyse_website.py
@@ -10,20 +10,31 @@ st.subheader("Analyse the Data")
 st.markdown("The next step was to do some anaylsis of the data and create some useful tools. The "
             "best book by an author came from this step, along with producing some insights in "
             "what to visulise.")
-st.markdown("**Here's the code for the best book by Author tool**")
-st.code('''df = pd.read_csv('data/analyse_this.csv')
+st.markdown("**Here's the code **")
+with st.beta_expander("See the code"):
+    st.code('''import pandas as pd
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    df = pd.read_csv('data/analyse_this.csv')
+    
+    def my_best_book(author):
+        if df['author'].str.contains(author).sum() > 0:
+            autors_books = df[df['author']== author]
+            rating = autors_books.loc[autors_books['norm_max_min'].idxmax()]
+            return rating.title
+        return "Author not found"
+    
+    def awards(df):
+        data=df
+        df = data.groupby("awards")["awards"].count()
+        print(df)
+    
+    
+    def original_publish_year(df):
+        data=df
+        dfs = data.groupby("original_publish_year")["num_ratings"].mean()
+        print(dfs)''')
 
-def my_best_book(author):
-    if df['author'].str.contains(author).sum() > 0:
-        autors_books = df[df['author']== author]
-        rating = autors_books.loc[autors_books['norm_max_min'].idxmax()]
-        return rating.title
-    return "Author not found"''')
-with st.beta_expander("See explanation"):
-            st.write(""" The code is actually pretty simple, using the power of pandas we can 
-            take the main data frame and author name input, then create a smaller dataframe with 
-            only the books written by that author. Then it is just the case of finding the one 
-            with the highest rating (usng min max norm rating)""")
-st.write("")
-st.write("")
-st.markdown("**If you would like to you can try the tool out in the sidebar**")
+
+
