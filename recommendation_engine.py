5a32ef4f7f84f0cb0b5864bed937409771ebd089
Joby Ingram-Dodd <joby@protonmail.com>
2021-01-14 17:34:52 +0000

    new website tweaks

---
+++ recommendation_engine.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import random
+
+def recoomend_a_book():
+    df = pd.read_csv('data/good_reads_df_web.csv')
+    t = df['Title']
+    title = random.choice(t)
+    dft = df[df['Title']==title]
+
+    return dft['Title'].values[0] + " by " + dft['Author'].values[0]
+
+print(recoomend_a_book())
--- website_badges.py
+++ website_badges.py
@@ -5,10 +5,26 @@ import numpy as np
 import pandas as pd
 import good_reads_analyse as gra
 import time
+import recommendation_engine as re
 
 #git hub badge
-st.markdown('![](<https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github'
-            '&logoColor=white>)')
+b1, b2 = st.beta_columns([1,3])
+with b1:
+    st.markdown('[![](<https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github'
+            '&logoColor=white>)](<https://github.com/jobyid/strive_build_good_reads>)')
 #python bagde
-st.markdown('![](https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python'
+with b2:
+    st.markdown('![](https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo'
+                     '=python'
             '&logoColor=white)')
+
+st.markdown('[![](<https://www.buymeacoffee.com/library/content/images/2020/09/image--67--1.png>)]('
+            '<https://www.buymeacoffee.com/joby>)')
+
+st.markdown("Need a recomendation? Use our recommeder below")
+st.text_input("Enter the last book you read:")
+if st.button("Reccomend"):
+    st.write("The science says you should read:")
+    with st.spinner("Wait For it"):
+        time.sleep(2)
+    st.write(re.recoomend_a_book())
